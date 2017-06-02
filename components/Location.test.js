9251940d2a84d71979c8a18c4efc7787108ff42a
Gabriel Bleu <gabriel.bleu@altares.com>
2017-06-02 21:15:23 +0200

    Location test only use shallow

--- components/Location.test.js
+++ components/Location.test.js
@@ -1,28 +1,28 @@
 /* eslint-env jest */
 
 import React from 'react';
-import { shallow, mount } from 'enzyme';
+import { shallow } from 'enzyme';
 
 import Location from './Location';
 
 describe('Location component', () => {
-  it('renders as expected', () => {
+  it('renders as expected', async () => {
     const wrapper = shallow(<Location />);
     expect(wrapper).toMatchSnapshot();
+
     wrapper.setState({ errorMessage: 'errorMessage' });
     expect(wrapper).toMatchSnapshot();
+
     wrapper.setState({ errorMessage: null, origin: { key: 'defense' } });
     expect(wrapper).toMatchSnapshot();
+
     wrapper.setState({ errorMessage: null, origin: { key: 'capitole' } });
     expect(wrapper).toMatchSnapshot();
+
     wrapper.setState({ departures: [new Date(0, 0, 0, 0, 0, 0, 0), null, null], traffic: 2 });
     expect(wrapper).toMatchSnapshot();
-  });
 
-  it('unmount as expected', () => {
     const remove = jest.fn();
-
-    const wrapper = mount(<Location />);
     wrapper.setState({ sub: { remove } });
     wrapper.unmount();
     expect(wrapper).toMatchSnapshot();
--- jestsetup.js
+++ jestsetup.js
@@ -2,19 +2,5 @@
 
 // eslint-disable-next-line import/no-extraneous-dependencies
 import fetchMock from 'jest-fetch-mock';
-// eslint-disable-next-line import/no-extraneous-dependencies
-import { jsdom } from 'jsdom';
 
 global.fetch = fetchMock;
-
-global.document = jsdom('');
-global.window = document.defaultView;
-Object.keys(document.defaultView).forEach((property) => {
-  if (typeof global[property] === 'undefined') {
-    global[property] = document.defaultView[property];
-  }
-});
-
-global.navigator = {
-  userAgent: 'node.js'
-};
