ed86a1d3f53193e29be3cecfb357519fd5335703
Gabriel Bleu <gabriel.bleu@altares.com>
2017-06-02 21:56:18 +0200

    Location add updateLocation test

--- components/Location.test.js
+++ components/Location.test.js
@@ -4,8 +4,20 @@ import React from 'react';
 import { shallow } from 'enzyme';
 
 import Location from './Location';
+import services from '../services';
 
 describe('Location component', () => {
+  beforeEach(() => {
+    jest.spyOn(services, 'guessOrigin');
+    services.getNextDepartures = jest.fn(() => [null, null, null]);
+    services.trafficBetween = jest.fn(() => 1);
+  });
+  afterEach(() => {
+    services.guessOrigin.mockRestore();
+    services.getNextDepartures.mockRestore();
+    services.trafficBetween.mockRestore();
+  });
+
   it('renders as expected', async () => {
     const wrapper = shallow(<Location />);
     expect(wrapper).toMatchSnapshot();
@@ -21,6 +33,13 @@ describe('Location component', () => {
 
     wrapper.setState({ departures: [new Date(0, 0, 0, 0, 0, 0, 0), null, null], traffic: 2 });
     expect(wrapper).toMatchSnapshot();
+
+    const julius = {
+      coords: { latitude: 48.888808, longitude: 2.2100604 }
+    };
+    await wrapper.instance().updateLocation(julius);
+    expect(wrapper).toMatchSnapshot();
+    expect(services.guessOrigin).toBeCalledWith(julius.coords);
 
     const remove = jest.fn();
     wrapper.setState({ sub: { remove } });
