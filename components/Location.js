7faa28c6013680497e3f0475a8c2b425cf529703
Gabriel Bleu <gabriel.bleu@altares.com>
2017-06-02 17:23:47 +0200

    fix location comp class name

--- components/Location.js
+++ components/Location.js
@@ -22,7 +22,7 @@ const styles = StyleSheet.create({
   }
 });
 
-export default class App extends Component {
+export default class LocationComp extends Component {
   state = {
     location: null,
     sub: null,
